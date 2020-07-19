ae2c0cd78be837d60f1f443f0f13f10cc4d39de3
Rohan Kumar <seirdy@protonmail.com>
2020-07-18 21:34:48 -0700

    Forecasting: add auto-ARIMA and model evaluation
    - Add wrapper for the auto-ARIMA forecasater from darts
    - Add `make_and_compare_predictions` tions to make and compare the
      accuracies of various forecasting models.
    
    To streamline `make_and_compare_predictions`, the types/classes
    `ModelParams` and `ModelsToMake` help specify many forecasting models
    with their parameters; `PredictionEvaluations` stores the various
    forecasts and their accuracies.

--- clogstats/forecasting/arima.py
+++ clogstats/forecasting/arima.py
@@ -1,20 +1,26 @@
 """Run ARIMA and ARIMA-related forecasts pre-fitted over activity stats."""
 
-from typing import NamedTuple
+from dataclasses import asdict, dataclass
+from typing import Optional
+
+import pandas as pd
 
 from darts import TimeSeries
-from darts.models.arima import ARIMA
+from darts.models.arima import ARIMA, AutoARIMA
 
 
 # first determine if the time series is stationary
 
 
-class ArimaParams(NamedTuple):
+@dataclass
+class ArimaParams:
     """Container for ARIMA parameters."""
 
-    p: int = 12  # noqa:VNE001,WPS111
-    d: int = 1  # noqa:VNE001,WPS111
-    q: int = 0  # noqa:VNE001,WPS111
+    # p, d, q are the standard 1-letter variable names for ARIMA.
+    # Disable linting for varnames.
+    p: int = 6  # noqa: VNE001,WPS111,C0103
+    d: Optional[int] = 0  # noqa: VNE001,WPS111,C0103  # assume data is stationary.
+    q: int = 0  # noqa: VNE001,WPS111,C0103
 
 
 def arima_analyzed_log(
@@ -32,3 +38,51 @@ def arima_analyzed_log(
     )  # optimize these values?
     model.fit(gathered_stats, component_index=component_index)
     return model
+
+
+_ONE_DAY = pd.Timedelta("1D")
+
+
+# Disabled linting for argcount because this function needs to pass a
+# large number of arguments to auto-ARIMA. I simplified what I could.
+def auto_arima_analyzed_log(  # noqa: WPS211  # Found too many arguments
+    *autoarima_args,
+    gathered_stats: TimeSeries,
+    start_arima_params: ArimaParams = None,
+    max_arima_params: ArimaParams = None,
+    component_index: int = None,
+    seasonal: bool = False,
+    seasonal_length: pd.Timedelta = _ONE_DAY,
+    **autoarima_kwargs,
+) -> AutoARIMA:
+    """ARIMA forecast with atuo-optimized params.
+
+    Wraps darts.models.arima.AutoARIMA.
+    """
+    if start_arima_params is None:
+        start_arima_params = ArimaParams(d=None)
+    if max_arima_params is None:
+        max_arima_params = start_arima_params
+        for param_name, start_value in asdict(max_arima_params).items():
+            try:
+                setattr(max_arima_params, param_name, max(start_value * 2, 1))
+            except TypeError:
+                setattr(max_arima_params, param_name, 1)
+    model = AutoARIMA(
+        start_p=start_arima_params.p,
+        d=start_arima_params.d,
+        start_q=start_arima_params.q,
+        max_p=max_arima_params.p,
+        max_d=max_arima_params.d,
+        max_q=max_arima_params.q,
+        seasonal=seasonal,
+        m=int(seasonal_length / gathered_stats.freq()),
+        *autoarima_args,
+        **autoarima_kwargs,
+    )
+    model.fit(gathered_stats, component_index=component_index)
+    return model
+
+
+# get to know ARIMA and auto.arima a bit more, look up AIC, BIC. Switch to stats
+# try for different channels
--- clogstats/forecasting/ts_utils.py
+++ clogstats/forecasting/ts_utils.py
@@ -1,10 +1,13 @@
 """Common utilities for time-series manipulation."""
 
-from typing import List
+from typing import Any, Callable, Dict, List, Mapping, NamedTuple
 
+import numpy as np
 import pandas as pd
 
 from darts import TimeSeries
+from darts.metrics import metrics
+from darts.models.forecasting_model import UnivariateForecastingModel  # for typing
 
 
 def create_timeseries(
@@ -19,4 +22,66 @@ def create_timeseries(
         value_cols = ["nicks", "msgs"]
     return TimeSeries.from_dataframe(
         ts_data, time_col=None, value_cols=value_cols, freq=freq,
+    )
+
+
+class ModelParams(NamedTuple):
+    """Contains a function to create a model and its arguments."""
+
+    prediction_function: Callable[..., UnivariateForecastingModel]
+    prediction_kwargs: Mapping[str, Any]
+
+
+ModelsToMake = Mapping[str, ModelParams]
+
+
+def make_forecasts(
+    train: TimeSeries, n_pred: int, predictions_to_make: ModelsToMake,
+) -> Dict[str, TimeSeries]:
+    """Run all the implemented predictions on a time series."""
+    models: Dict[str, UnivariateForecastingModel] = {
+        name: model.prediction_function(train, **model.prediction_kwargs)
+        for (name, model) in predictions_to_make.items()
+    }
+    return {name: model.predict(n_pred) for name, model in models.items()}
+
+
+Reduction = Callable[[np.ndarray], float]
+Metric = Callable[[TimeSeries, TimeSeries, bool, Reduction], float]
+
+
+def compare_predictions(
+    actual: TimeSeries, predictions: Dict[str, TimeSeries], metric: Metric,
+) -> Dict[str, float]:
+    """Compare multiple predictions and sort them by the given error metric."""
+    prediction_metrics = {
+        name: metric(actual, prediction) for (name, prediction) in predictions.items()
+    }
+    return dict(sorted(prediction_metrics.items(), key=lambda pair: pair[1]))
+
+
+class PredictionEvaluations(NamedTuple):
+    """Container for predictions and their accuracies."""
+
+    predictions: Mapping[str, TimeSeries]
+    evaluations: Mapping[str, float]
+
+
+_ONE_DAY: pd.Timedelta = pd.Timedelta("1D")
+
+
+def make_and_compare_predictions(
+    gathered_stats: TimeSeries,
+    predictions_to_make: ModelsToMake,
+    prediction_duration: pd.Timedelta = _ONE_DAY,
+    metric: Metric = metrics.mse,
+) -> PredictionEvaluations:
+    """Run multiple forecasts and compare their accuracy."""
+    train, actual = gathered_stats.split_after(
+        gathered_stats.end_time() - prediction_duration,
+    )
+    forecasts = make_forecasts(train, len(actual), predictions_to_make)
+    return PredictionEvaluations(
+        predictions=forecasts,
+        evaluations=compare_predictions(actual, forecasts, metric),
     )
