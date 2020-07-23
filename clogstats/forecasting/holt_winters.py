1fe974cac998cbf9d517bd7cde244ecf2d3890ee
Rohan Kumar <seirdy@protonmail.com>
2020-07-23 11:22:32 -0700

    Feat: pass unpacked kwargs to ExponentialSmoothing
    ExponentialSmoothing (and the statsmodels function it wraps) take many
    keyword args that forecasting.holt_winters.hw_analyzed_log() previously
    didn't accept. This commit allows specifying arbitrary unpacked kwargs
    to pass to these functions.

--- clogstats/forecasting/holt_winters.py
+++ clogstats/forecasting/holt_winters.py
@@ -1,5 +1,7 @@
 """Run triple-exponential smoothing forecasts, pre-fitted over activity stats."""
 
+from typing import Any, Mapping
+
 import pandas as pd
 from darts import TimeSeries
 from darts.models.exponential_smoothing import ExponentialSmoothing
@@ -9,7 +11,8 @@ def hw_analyzed_log(
     gathered_stats: TimeSeries,
     seasonal_periods: int = 0,
     seasonal_length: pd.Timedelta = None,
-    component_index: int = None,
+    component_index: int = 0,
+    **exponential_smoothing_kwargs: Mapping[str, Any],
 ) -> ExponentialSmoothing:
     """Create a pre-fitted ExponentialSmoothing object from IRC log stats.
 
@@ -19,6 +22,8 @@ def hw_analyzed_log(
         seasonal_periods = max(
             seasonal_periods, int(gathered_stats.duration() / seasonal_length),
         )
-    model = ExponentialSmoothing(seasonal="mul", seasonal_periods=seasonal_periods)
+    model = ExponentialSmoothing(
+        seasonal_periods=seasonal_periods, **exponential_smoothing_kwargs,
+    )
     model.fit(gathered_stats, component_index=component_index)
     return model
